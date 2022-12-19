b46e5394e023c949a0797dbd48522962f8eac3fa
Samoshin-Egor <121015765+Samoshin-Egor@users.noreply.github.com>
2022-12-19 13:54:59 +0300

    obolochka2.java

---
+++ obolochka2.java
@@ -0,0 +1,9 @@
+package Practice3;
+
+public class obolochka2 {
+    public static void main(String [] args){
+        String a = "123212";
+        double x = Double.parseDouble(a);
+        System.out.println(x);
+    }
+}
