95d5ae720c436bc96012cf1938ba8d67d0731581
Samoshin-Egor <121015765+Samoshin-Egor@users.noreply.github.com>
2022-12-19 14:00:15 +0300

    obolochka1.java

---
+++ obolochka1.java
@@ -0,0 +1,15 @@
+package Practice3;
+
+public class obolochka_double1 {
+    public static void main(String[]args){
+        int a = 25;
+        double b = 12.12;
+        String c = "123432";
+        double x = Double.valueOf(a);
+        System.out.println(x);
+        x = Double.valueOf(b);
+        System.out.println(x);
+        x = Double.valueOf(c);
+        System.out.println(x);
+    }
+}
